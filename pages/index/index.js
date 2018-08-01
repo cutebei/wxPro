4059538a1eb9d07b63eaa5a5fd773d5391e087fa
cutebei <m18811443954@163.com>
2018-08-01 10:54:02 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,16 +3,90 @@
 var base = getApp();
 Page({
   data: {
-    path:base.path.res+"smallexe/index/",
+    path: base.path.res + "smallexe/index/",
     motto: '你好',
     userInfo: {},
     array: ['上海', '北京', '杭州', '宁波'],
+    s_info: [
+      {
+        src: '/pages/index/images/bg1.png',
+        money: '80',
+        proice: '21947',
+        jianjie: '五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷',
+        diqu: '东北',
+        cheng: '黑龙江'
+      },
+      {
+        src: '/pages/index/images/bg1.png',
+        money: '80',
+        proice: '21947',
+        jianjie: '五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷',
+        diqu: '东北',
+        cheng: '黑龙江'
+      },
+      {
+        src: '/pages/index/images/bg1.png',
+        money: '80',
+        proice: '21947',
+        jianjie: '五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷',
+        diqu: '东北',
+        cheng: '黑龙江'
+      },
+      {
+        src: '/pages/index/images/bg1.png',
+        money: '80',
+        proice: '21947',
+        jianjie: '五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷五谷',
+        diqu: '东北',
+        cheng: '黑龙江'
+      },
+    ],
+    events: [
+      {
+        img: "../../icon/home-2.png", name: "拼团"
+      },
+      {
+        img: "../../icon/home-2.png", name: "抽奖",
+      },
+      {
+        img: "../../icon/home-2.png", name: "抢购",
+      },
+      {
+        img: "../../icon/home-2.png", name: "优惠券",
+      },
+      {
+        img: "../../icon/home-2.png", name: "在线预订",
+      }
+    ],
+    ptArr: [
+      {
+        img: base.path.res + "smallexe/index/jd.jpg", naturl: "30", num: "2", cheap: "28"
+      },
+      {
+        img: base.path.res + "smallexe/index/001.jpg", naturl: "30", num: "3", cheap: "28"
+      }
+    ],
+    jgArrLeft: [
+      {
+        img: base.path.res + "smallexe/index/jd.jpg", name: "10点场", hour: "00", min: "36", sec: "22"
+      }
+    ],
+    jgArrRight: [
+      {
+        imgArr: [{
+          img: base.path.res + "smallexe/index/jd.jpg",
+        },
+        { img: base.path.res + "smallexe/index/jd.jpg", }],
+        title:"有好货",
+        con:"卖家推荐"
+      },
+    ],
     index: 0,
     imgUrls: [
 
-      {picurl:'https://p3.pstatp.com/large/43700001e49d85d3ab52',},
+      { picurl: 'https://p3.pstatp.com/large/43700001e49d85d3ab52', },
 
-      {picurl: 'https://p3.pstatp.com/large/39f600038907bf3b9c96',} ,
+      { picurl: 'https://p3.pstatp.com/large/39f600038907bf3b9c96', },
 
       { picurl: 'https://p3.pstatp.com/large/31fa0003ed7228adf421', }
 
@@ -30,8 +104,8 @@ Page({
   },
   goCake: function (e) {
     var brand = e.currentTarget.dataset.brand;
-    if(brand&&brand==1){
-      base.cake.tab=1;
+    if (brand && brand == 1) {
+      base.cake.tab = 1;
     }
     wx.switchTab({ url: '../cake/cake' });
   },
@@ -39,7 +113,7 @@ Page({
     var nm = e.currentTarget.dataset.pname;
     var b = e.currentTarget.dataset.brand;
     wx.navigateTo({
-      url: '../cakeDetail/cakeDetail?pname=' + nm+"&brand="+(b||0)
+      url: '../cakeDetail/cakeDetail?pname=' + nm + "&brand=" + (b || 0)
     })
   },
   bindPickerChange: function (e) {
@@ -87,13 +161,13 @@ Page({
     //   }
     // })
   },
-    //调用应用实例的方法获取全局数据
-    //app.getUserInfo(function (userInfo) {
-    //更新数据
-    //that.setData({
-    //userInfo: userInfo
-    //})
-    //})
+  //调用应用实例的方法获取全局数据
+  //app.getUserInfo(function (userInfo) {
+  //更新数据
+  //that.setData({
+  //userInfo: userInfo
+  //})
+  //})
 
   onPullDownRefresh: function () {
     wx.stopPullDownRefresh()
